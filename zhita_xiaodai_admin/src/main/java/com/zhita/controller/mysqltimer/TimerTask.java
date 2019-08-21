35e877c47d606371272e9563e765aff36ae5308e
MicroWin10-1201\Administrator <2454907255@qq.com>
2019-08-21 10:24:42 +0800

    lhq

--- zhita_xiaodai_admin/src/main/java/com/zhita/controller/mysqltimer/TimerTask.java
+++ zhita_xiaodai_admin/src/main/java/com/zhita/controller/mysqltimer/TimerTask.java
@@ -23,7 +23,7 @@ public class TimerTask {
      */
 	  //                   秒分时日 月周
 	 //每日0点  将各个渠道的历史数据存入历史表
-      @Scheduled(cron = "0 0 0 * * ?")
+      @Scheduled(cron = "0 01 0 * * ?")
       public void test1() throws ParseException
       {
           System.out.println("开始做定时任务1");
@@ -33,7 +33,7 @@ public class TimerTask {
       
       //                   秒分时日 月周
       //定时任务     控制逾期
-      @Scheduled(cron = "0 0 10,0 * * ?")
+      @Scheduled(cron = "0 01 0 * * ?")
       public void test2() throws ParseException
       {
           System.out.println("开始做定时任务2");
@@ -43,7 +43,7 @@ public class TimerTask {
       
       //                   秒分时日 月周
       //定时任务   控制逾期等级
-      @Scheduled(cron = "0 0 0 * * ?")
+      @Scheduled(cron = "0 01 0 * * ?")
       public void test3() throws ParseException
       {
           System.out.println("开始做定时任务3");
@@ -53,7 +53,7 @@ public class TimerTask {
       
       //                   秒分时日 月周
       //定时任务   控制黑名单
-      @Scheduled(cron = "0 0 0 * * ?")
+      @Scheduled(cron = "0 01 0 * * ?")
       public void test4() throws ParseException
       {
           System.out.println("开始做定时任务4");
