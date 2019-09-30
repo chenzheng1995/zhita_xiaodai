c3f692a69c37313976cbafbe107abb4eca624412
MicroWin10-1201\Administrator <2454907255@qq.com>
2019-09-30 16:31:33 +0800

    lhq

--- zhita_xiaodai_service/src/main/java/com/zhita/service/manage/source/SourceServiceImp.java
+++ zhita_xiaodai_service/src/main/java/com/zhita/service/manage/source/SourceServiceImp.java
@@ -98,7 +98,7 @@ public class SourceServiceImp implements IntSourceService{
     	String templateName = record.getName();
     	Integer templateId = sourceTemplateMapper.getid(templateName);
     	record.setTemplateid(templateId);
-    	record.setLink("http://dhj.rong51dai.com/template/"+templateName+"/index.html?code="+record.getSourcename()+"&token="+record.getToken());
+    	record.setLink("https://mdb.tcc1688.com/template/"+templateName+"/index.html?code="+record.getSourcename()+"&token="+record.getToken());
     	
     	int count=sourceMapper.ifSourceNameIfExist(record.getSourcename());
 		int num=0;
@@ -132,7 +132,7 @@ public class SourceServiceImp implements IntSourceService{
     	String templateName = record.getName();
     	Integer templateId = sourceTemplateMapper.getid(templateName);
     	record.setTemplateid(templateId);
-    	record.setLink("http://dhj.rong51dai.com/template/"+templateName+"/index.html?code="+record.getSourcename()+"&token="+record.getToken());
+    	record.setLink("https://mdb.tcc1688.com/template/"+templateName+"/index.html?code="+record.getSourcename()+"&token="+record.getToken());
     	
     	String discount=sourceMapper.queryDiscountById(record.getId());//得到修改之前的那个折扣率  （比如取到字符串  "80%"）
 		RedisClientUtil redisClientUtil = new RedisClientUtil();
