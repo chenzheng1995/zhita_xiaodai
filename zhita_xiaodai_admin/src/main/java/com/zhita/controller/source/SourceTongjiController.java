c92ef4b7cca6d3039199b7c4f105d18e884ff563
MicroWin10-1201\Administrator <2454907255@qq.com>
2019-10-23 10:57:58 +0800

    lhq

--- zhita_xiaodai_admin/src/main/java/com/zhita/controller/source/SourceTongjiController.java
+++ zhita_xiaodai_admin/src/main/java/com/zhita/controller/source/SourceTongjiController.java
@@ -230,11 +230,11 @@ public class SourceTongjiController {
 				}
 			}*/
 			
-			int authencount=intSourceService.queryattcount(sourceid, startTimestamps, endTimestamps);//认证人数
+			int authencount=intSourceService.queryattcount(sourceids, startTimestamps, endTimestamps);//认证人数
 			listsource.get(i).setAuthencount(authencount);//当前渠道的认证人数（用户个人信息认证表）
-			int authenbankcount=intSourceService.querybankcount(sourceid, startTimestamps, endTimestamps);//认证人数
+			int authenbankcount=intSourceService.querybankcount(sourceids, startTimestamps, endTimestamps);//认证人数
 			listsource.get(i).setAuthenbankcount(authenbankcount);//当前渠道的认证人数（银行卡认证表）
-			int authenoperacount=intSourceService.queryoperacount(sourceid, startTimestamps, endTimestamps);//认证人数
+			int authenoperacount=intSourceService.queryoperacount(sourceids, startTimestamps, endTimestamps);//认证人数
 			listsource.get(i).setAuthenoperacount(authenoperacount);//当前渠道的认证人数（运营商认证表）
 			
 			Integer applynum=intSourceService.queryNum(companyId, sourceids,startTimestamps, endTimestamps);//申请人数
