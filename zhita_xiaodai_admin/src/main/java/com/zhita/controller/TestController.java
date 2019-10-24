c34e8d2cd9d20de763744a72b607e34b1e3d8847
MicroWin10-1201\Administrator <2454907255@qq.com>
2019-10-24 16:55:05 +0800

    lhq

--- zhita_xiaodai_admin/src/main/java/com/zhita/controller/TestController.java
+++ zhita_xiaodai_admin/src/main/java/com/zhita/controller/TestController.java
@@ -8,8 +8,10 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -19,11 +21,14 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.zhita.dao.manage.SourceDiscountHistoryMapper;
+import com.zhita.dao.manage.SourceMapper;
 import com.zhita.model.manage.User;
 import com.zhita.service.manage.login.IntLoginService;
 import com.zhita.service.test.TestService;
 import com.zhita.util.PhoneDeal;
 import com.zhita.util.RedisClientUtil;
+import com.zhita.util.Timestamps;
 import com.zhita.util.TuoMinUtil;
 
 
@@ -36,6 +41,10 @@ public class TestController {
 	TestService testService;
 	@Autowired
 	private IntLoginService intLoginService;
+	@Autowired
+	private SourceMapper sourceMapper;
+	@Autowired
+	private SourceDiscountHistoryMapper sourceDiscountHistoryMapper;
 
 	
 	@RequestMapping("/settest")
@@ -226,9 +235,72 @@ public class TestController {
 		System.out.println(new Date().getTime()+"+++++++++++++++++++");
 		
 		
-		User u=new User();
-		u.setId(1);
-		u.setAccount("test");
-		System.out.println("-------------"+u.getClass().getDeclaredFields().length);
+		List<String> list1=new ArrayList<>();
+		list1.add("2019-10-18");
+		list1.add("2019-10-19");
+		list1.add("2019-10-20");
+		list1.add("2019-10-21");
+		list1.add("2019-10-22");
+		list1.add("2019-10-23");
+		List<String> list2=new ArrayList<>();
+		list2.add("2019-10-18");
+		list2.add("2019-10-19");
+		list2.add("2019-10-20");
+		list2.add("2019-10-21");
+		list2.add("2019-10-22");
+		List<String> intersectionlist=list1.stream().filter(t-> !list2.contains(t)).collect(Collectors.toList());//差集
+		intersectionlist.stream().forEach(System.out::println);
+		for (int i = 0; i < intersectionlist.size(); i++) {
+			System.out.println("intersectionlist:"+intersectionlist.get(i)+"main方法测试");
+		}
+		
 	}	
+	
+	@ResponseBody
+	@RequestMapping("/lhqtest")
+	public void test(){
+		Integer companyId=3;
+		String sourceName="CS888";
+		Integer sourceid=65;
+			List<String> liststr=sourceMapper.queryTime(companyId, sourceName);// 查询出当前渠道所有的注册时间(liststr里面的时间为时间戳格式)
+			if(liststr!=null&&!liststr.isEmpty()) {//代表当前渠道在用户表有注册的用户
+				List<String> list1 = new ArrayList<>();// 用来存时间戳转换后的时间（年月日格式的时间）(user的注册时间)
+				for (int j = 0; j< liststr.size(); j++) {
+					list1.add(Timestamps.stampToDate1(liststr.get(j)));
+				}
+				HashSet h = new HashSet(list1);
+				list1.clear();
+				list1.addAll(h);
+				
+				Date d=new Date();
+				SimpleDateFormat sf=new SimpleDateFormat("yyyy-MM-dd");
+				String date=sf.format(d);//date为当天时间(格式为年月日)
+				
+				list1.remove(date);//将当天时间从list1中移除
+				List<String> listdate=sourceDiscountHistoryMapper.queryDate(sourceid);//当前渠道在历史表的时间（listdate里面的时间为时间戳格式）
+				
+				List<String> list2 = new ArrayList<>();// 用来存时间戳转换后的时间（年月日格式的时间）（历史表的时间）
+				for (int k = 0; k < listdate.size(); k++) {
+					list2.add(Timestamps.stampToDate1(listdate.get(k)));
+				}
+				HashSet h1 = new HashSet(list2);
+				list2.clear();
+				list2.addAll(h1);
+				
+				List<String> intersectionlist=list1.stream().filter(t-> !list2.contains(t)).collect(Collectors.toList());//差集
+				intersectionlist.stream().forEach(System.out::println);
+				
+				for (int i = 0; i < list1.size(); i++) {
+					System.out.println("list1:"+list1.get(i));
+				}
+				System.out.println("-------------------");
+				for (int i = 0; i < list2.size(); i++) {
+					System.out.println("list2:"+list2.get(i));
+				}
+				System.out.println("-------------------");
+				for (int i = 0; i < intersectionlist.size(); i++) {
+					System.out.println("intersectionlist:"+intersectionlist.get(i));
+				}
+		}
+	}
 }
