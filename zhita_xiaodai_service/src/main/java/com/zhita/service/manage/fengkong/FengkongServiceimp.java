c4d0d2abf905430de5a128b85aa738f556bbc17d
MicroWin10-1201\Administrator <2454907255@qq.com>
2019-10-29 18:06:12 +0800

    lhq

--- zhita_xiaodai_service/src/main/java/com/zhita/service/manage/fengkong/FengkongServiceimp.java
+++ zhita_xiaodai_service/src/main/java/com/zhita/service/manage/fengkong/FengkongServiceimp.java
@@ -1,10 +1,12 @@
 package com.zhita.service.manage.fengkong;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.zhita.dao.manage.FengkongMapper;
 
 
-
+@Service
 public class FengkongServiceimp implements FengkongService{
 
 	
